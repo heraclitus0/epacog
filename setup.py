e125ff080e1b09875bb2ec3000d520b42bc2db5f
bharadwaj <bharadwajpulikanti11@gmail.com>
2025-07-29 08:44:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 with open("README.md", encoding="utf-8") as f:
-    long_desc = f.read()
+    long_description = f.read()
 
 setup(
     name='epacog',
     version='0.1.0',
     description='Recursive Epistemic Control under Drift, Rupture, and Collapse',
-    long_description = open("README.md", encoding="utf-8").read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     author='Pulikanti Sashi Bharadwaj',
     author_email='bharadwajpulikanti11@gmail.com',
